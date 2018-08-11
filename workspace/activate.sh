68f2fad5d737b436abae06f2789e4c146ee75cce
Marco Puccio <mpuccio90@gmail.com>
2018-08-11 00:04:52 +0000

    dynamic key

--- workspace/activate.sh
+++ workspace/activate.sh
@@ -7,12 +7,15 @@ export PYTHONPATH="$ROOT"
 
 alias app="$APP"
 
-export DEBUG='True'
-export SECRET_KEY='Your Django Secret Key'
+if [ -z "$SECRET_KEY" ]; then
+  export SECRET_KEY=`exec python $ROOT/workspace/keygen.py`
+fi
 
-export DATABASE_NAME='Your Project Database Name'
-export DATABASE_USER='Your Database User'
-export DATABASE_PASSWORD='Your Database Password'
+export DEBUG='True'
+
+export DATABASE_NAME=''
+export DATABASE_USER=''
+export DATABASE_PASSWORD=''
 
 export STATIC_URL='/static/'
 export STATIC_ROOT='static'
@@ -20,8 +23,9 @@ export MEDIA_URL='/media/'
 export MEDIA_ROOT='media'
 
 
+
 if [ ! -f workspace/local.sh ]; then
-    echo $'echo \"Local constants\"' > workspace/local.sh
-    chmod o+x workspace/local.sh
+  echo $'echo \"Local constants\"' > workspace/local.sh
+  chmod o+x workspace/local.sh
 fi
 . $ROOT/workspace/local.sh
